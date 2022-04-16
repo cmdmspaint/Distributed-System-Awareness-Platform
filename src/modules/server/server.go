8815f4b1bf60c886139bb858fb4cdd5ed1d0afb3
zhanlinmao <1711517483@qq.com>
2022-04-16 21:43:34 +0800

    init

--- src/modules/server/server.go
+++ src/modules/server/server.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"Distributed-System-Awareness-Platform/src/models"
+	"Distributed-System-Awareness-Platform/src/modules/server/config"
 	"fmt"
-	_ "gopkg.in/alecthomas/kingpin.v2"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/prometheus/common/promlog"
+	prometheuslogflag "github.com/prometheus/common/promlog/flag"
+	"github.com/prometheus/common/version"
+	"gopkg.in/alecthomas/kingpin.v2"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 var (
@@ -15,11 +24,58 @@ var (
 )
 
 func main() {
-	// 版本信息
-	app.Version("1.0")
-	// 帮助信息
+
+	app.Version(version.Print("open-devops-server"))
+
 	app.HelpFlag.Short('h')
+
+	prometheusLogConfig := promlog.Config{}
+
+	prometheuslogflag.AddFlags(app, &prometheusLogConfig)
 	// 强制解析
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 	fmt.Println(*configFile)
+
+	// 设置logger
+	var logger log.Logger
+	logger = func(config *promlog.Config) log.Logger {
+		var (
+			l  log.Logger
+			le level.Option
+		)
+		if config.Format.String() == "logfmt" {
+			l = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+		} else {
+			l = log.NewJSONLogger(log.NewSyncWriter(os.Stderr))
+		}
+
+		switch config.Level.String() {
+		case "debug":
+			le = level.AllowDebug()
+		case "info":
+			le = level.AllowInfo()
+		case "warn":
+			le = level.AllowWarn()
+		case "error":
+			le = level.AllowError()
+		}
+		l = level.NewFilter(l, le)
+		l = log.With(l, "ts", log.TimestampFormat(
+			func() time.Time { return time.Now().Local() },
+			"2006-01-02T15:04:05.000Z07:00",
+		), "caller", log.DefaultCaller)
+		return l
+	}(&prometheusLogConfig)
+	level.Info(logger).Log("msg", "using config.file", "file.path", *configFile)
+
+	//读取配置文件
+	sConfig, err := config.LoadFile(*configFile)
+	if err != nil {
+		level.Error(logger).Log("msg", "config.LoadFile Error,Exiting ...", "error", err)
+		return
+	}
+	level.Info(logger).Log("msg", "load.config.success", "file.path", *configFile, "content.mysql.num", len(sConfig.MysqlS))
+
+	models.InitMySQL(sConfig.MysqlS)
+	level.Info(logger).Log("msg", "load.mysql.success", "db.num", len(models.DB))
 }
